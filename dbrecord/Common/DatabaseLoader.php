36bae3c4069ab59d7c1edd2e911db57a7e8a8a3a
Roman Matěna <info@romanmatena.cz>
2014-02-26 22:54:54 +0100

    concept

--- dbrecord/Common/DatabaseLoader.php
+++ dbrecord/Common/DatabaseLoader.php
@@ -12,16 +12,16 @@ namespace dbrecord;
 class DatabaseLoader
 {
 
+	/** @var \Nette\DI\Container */
+	protected $context;
+
+
 	/** @var string */
-	public $tempDir;
+	protected $tempDir;
 
 
 	/** @var array */
-	public $docComment = array();
-
-
-	/** @var \Nette\DI\Container */
-	public $context;
+	protected $comments = array();
 
 
 	/** @var array */
@@ -40,29 +40,45 @@ class DatabaseLoader
 
 
 
-	public function createClasses()
+	public function setTempDir($tempDir)
+	{
+		$this->tempDir = $tempDir;
+	}
+
+
+
+
+
+	public function setComments($comments)
+	{
+		$this->comments = $comments;
+	}
+
+
+
+
+
+	protected function prepareTables()
 	{
 		$sql = $this->getConnection();
 		$db = $this->getDatabaseInfo();
-		foreach($db->getTables() as $table) {
-			$entity = array();			
+		foreach ($db->getTables() as $table) {
+			$entity = array();
 			$className = str_replace("\\\\", "\\", implode(
-							'\\', 
-							array_map(
-								function($value) {
-									return ucfirst($value);
-								}, 
-								explode("_", $table->getName())
+							'\\', array_map(
+									function($value) {
+								return ucfirst($value);
+							}, explode("_", $table->getName())
 							)
-						));
-			
+			));
+
 			// table comment?
 			$status = $sql->query("SHOW TABLE STATUS LIKE %s", $table->getName())->fetch();
-			if (isset($status['Comment']) && ($comment = $status['Comment'])) {				
+			if (isset($status['Comment']) && ($comment = $status['Comment'])) {
 				preg_match('/^@Entity\((.*)\)/', $comment, $matches);
 				if (isset($matches[1])) {
 					$matches = explode(",", $matches[1]);
-					foreach($matches as $value) {
+					foreach ($matches as $value) {
 						$v = explode("=", $value);
 						$v1 = trim($v[1], '"\'');
 						if ($v[0] == "name") {
@@ -74,11 +90,32 @@ class DatabaseLoader
 					}
 				}
 			}
-			
-			$this->buildEntityClass($className, $table, $entity);			
+
+			// first store all tables
+			$this->cache['tables'][$table->getName()] = array(
+				'className' => $className,
+				'table' => $table,
+				'entity' => $entity,
+			);
 		}
-		
 	}
+
+
+
+
+
+	public function createClasses()
+	{
+		$this->prepareTables();
+		// now go through
+		foreach ($this->cache['tables'] as $v) {
+			$this->buildEntityClass($v['className'], $v['table'], $v['entity']);
+		}
+	}
+
+
+
+
 
 	public function buildEntityClass($className, $table, $entity = array())
 	{
@@ -87,135 +124,203 @@ class DatabaseLoader
 		if (!is_dir(dirname($path))) {
 			mkdir(dirname($path), 0777, true);
 		}
-		
+
 		$fh = fopen($path . ".php", "w");
 		fwrite($fh, $this->generateEntityClass($className, $table, $entity));
 		fclose($fh);
 	}
 
 
-	
+
+
+
 	public function generateEntityClass($className, $table, $entity = array())
 	{
 		$namespace = strpos($className, "\\") ? ltrim(dirname($className), "\\") : NULL;
-		
-		$ret  = '<?php' . "\n\n\n";
-		
+
+		$ret = '<?php' . "\n\n\n";
+
 		if ($namespace) {
 			$ret .= "\n";
 			$ret .= "namespace " . $namespace . ";";
 			$ret .= "\n";
 		}
-		
+
 		$ret .= "\n";
 		$ret .= 'use dbrecord\Entity;' . "\n";
 		$ret .= "\n";
 
-		$ret .= $this->generateDocComment($className, $table, $entity);
+		$ret .= $this->generateEntityComment($className, $table, $entity);
 
 		$ret .= 'class ' . basename($className) . ' extends Entity' . "\n";
 		$ret .= '{' . "\n";
 		$ret .= "\n";
 		$ret .= '}' . "\n";
-		
+
 		return $ret;
 	}
 
-	
 
 
-	protected function generateDocComment($className, $table, $entity = array(), $replacement = array())
+
+
+	protected function generateClassDescription($className)
 	{
-		$entitySerialized = array();
-		if (count($entity)) {
-			foreach($entity as $k => $v) {
-				$entitySerialized[] = $k . "=\"" . $v . "\"";
+		$ret = "";
+		if (!isset($this->comments['description'])) {
+			$ret .= " * " . basename($className) . "\n";
+		}
+		else {
+			$ret .= " * " . $this->comments['description'] . "\n";
+		}
+
+		$ret .= " *\n";
+
+		$order = array("@author", "@copyright");
+		foreach ($order as $v) {
+			if (isset($this->comments[$v])) {
+				$ret .= " * " . $v . " " . $this->comments[$v] . "\n";
 			}
 		}
-		
-		$ret  = "/**\n";
-		
-		$commentOrder = array("description", "@author", "@copyright");
-		foreach ($commentOrder as $v) {
-			if (isset($replacement[$v])) {
-				$ret .= " * " . $replacement[$v] . "\n *\n";
-			}
-			elseif ($v == "description") {
-				if (!isset($this->docComment['description'])) {
-					$ret.=" * " . basename($className) . "\n *\n";
-				}
-				else {
-					$ret.=" * " . $this->docComment['description'] . "\n *\n";
-				}
-			}
-			elseif (isset($this->docComment[$v])) {
-				$ret.=" * " . $v . " " . $this->docComment[$v] . "\n";
-			}
+
+		return $ret;
+	}
+
+
+
+
+
+	protected function generateEntityDescription($table, $entity)
+	{
+		$ret = "";
+		$ret .= " * @Entity(" . $this->serializeArray($entity) . ")\n";
+		$ret .= " * @Table(name=\"" . $table->getName() . "\")\n";
+
+		return $ret;
+	}
+
+
+
+
+
+	protected function generateEntityProperties($table)
+	{
+		$ret = "";
+
+		// find primary keys
+		$pks = array();
+		foreach ($table->getPrimaryKey()->getColumns() as $column) {
+			$pks[] = $column->getName();
 		}
-		
-		$ret .= " *\n";
-		
-		$ret .= " * @Entity(" . implode(",", $entitySerialized) . ")\n";
-		$ret .= " * @Table(name=\"" . $table->getName() . "\")\n";		
-		
-		$ret .= " *\n";
-		
+
 		// php nezná datové typy time (d,t) naopak bool (b) nezná mysql, dostupné typy pro anotace jsou string, int, float
 		foreach ($table->getColumns() as $column) {
-			$type = $column->getType();
-			$name = $column->getName();
-			switch ($type) {
-				case "s":
-					$type = "string";
-					break;
-				
-				case "i":
-					$type = "int";
-					break;
-				
-				case "f":
-					$type = "float";
-					break;
-				
-				case "d":
-				case "t":
-					$type = "DateTime"; // dibi s nastavením setTypes() u fetch vždy vrátí int, převádí pomocí fce strtotime datum na int
-					break;
-			}
-			
-			$ret.=' * @property ' . $type . ' $' . $name . "\n";
+			$ret .= $this->generateEntityProperty($table, $column, $pks);
 		}
-		$ret .= " *\n";
-/*
-		if (isset($this->config['associations'])) {
-			$hasOne = $hasMany = array();
-			$ret.=" *\n";
-			foreach ($this->config['associations'] as $key => $association) {
-				if ($association['type'] == "hasOne") {
-					$hasOne[] = $key;
 
-					$ret.=' * @property ' . $association['referenceClass'] . ' $' . $key . "\n";
-				}
-				elseif ($association['type'] == "hasMany") {
-					$hasMany[] = $key;
-
-					$ret.=' * @property ' . (isset($association['associatedCollection']) ? $association['associatedCollection'] : "AssociatedCollection") . ' $' . $key . "\n";
-				}
-			}
-			$ret.=" *\n";
-			if (count($hasOne)) {
-				$ret.=' * @hasOne (' . implode(", ", $hasOne) . ')' . "\n";
-			}
-			if (count($hasMany)) {
-				$ret.=' * @hasMany (' . implode(", ", $hasMany) . ')' . "\n";
-			}
-			$ret.=" *\n";
-		}
-*/
-		$ret .= " */\n";
-		
 		return $ret;
 	}
+
+
+	protected function generateEntityAssociations($table)
+	{
+		$ret = "";
+
+		/*
+		  if (isset($this->config['associations'])) {
+		  $hasOne = $hasMany = array();
+		  $ret.=" *\n";
+		  foreach ($this->config['associations'] as $key => $association) {
+		  if ($association['type'] == "hasOne") {
+		  $hasOne[] = $key;
+
+		  $ret.=' * @property ' . $association['referenceClass'] . ' $' . $key . "\n";
+		  }
+		  elseif ($association['type'] == "hasMany") {
+		  $hasMany[] = $key;
+
+		  $ret.=' * @property ' . (isset($association['associatedCollection']) ? $association['associatedCollection'] : "AssociatedCollection") . ' $' . $key . "\n";
+		  }
+		  }
+		  $ret.=" *\n";
+		  if (count($hasOne)) {
+		  $ret.=' * @hasOne (' . implode(", ", $hasOne) . ')' . "\n";
+		  }
+		  if (count($hasMany)) {
+		  $ret.=' * @hasMany (' . implode(", ", $hasMany) . ')' . "\n";
+		  }
+		  $ret.=" *\n";
+		  }
+		 */
+
+
+		return $ret;
+	}
+
+
+
+
+	protected function generateEntityComment($className, $table, $entity = array())
+	{
+		$ret = "/**\n";
+		$ret .= $this->generateClassDescription($className);
+		$ret .= " *\n";
+		$ret .= $this->generateEntityDescription($table, $entity);
+		$ret .= " *\n";
+		$ret .= $this->generateEntityProperties($table);
+		$ret .= " *\n";
+		$ret .= $this->generateEntityAssociations($table);
+		$ret .= " *\n";
+		$ret .= " */\n";
+
+		return $ret;
+	}
+
+
+
+
+
+	protected function generateEntityProperty($table, \DibiColumnInfo $column, $pks)
+	{
+		$name = $column->getName();
+		
+		$params = array(
+			'type' => $type = $column->getType(),
+			'size' => $column->getSize(),
+			'nullable' => $column->isNullable() ? "true" : "false",
+			'default' => is_null($default = $column->getDefault()) ? "NULL" : $default,
+		);
+
+		if (in_array($name, $pks)) {
+			$params['primary'] = "true";
+			if ($column->isAutoincrement()) {
+				$params['autoincrement'] = "true";
+			}
+		}
+		
+		$propertyType = NULL;
+		switch ($type) {
+			case "s":
+				$propertyType = "string";
+				break;
+
+			case "i":
+				$propertyType = "int";
+				break;
+
+			case "f":
+				$propertyType = "float";
+				break;
+
+			case "d":
+			case "t":
+				$propertyType = "\dbrecord\DateTime"; // dibi s nastavením setTypes() u fetch vždy vrátí int, převádí pomocí fce strtotime datum na int
+				break;
+		}
+
+		return ' * @property ' . $propertyType . ' $' . $name . "	Column(" . $this->serializeArray($params) . ")\n";
+	}
+
 
 
 
@@ -245,17 +350,16 @@ class DatabaseLoader
 
 
 
-
-
-
-
-
-	protected function getTableColumns()
+	protected function serializeArray($array)
 	{
-		if (!isset($this->cache['databaseInfo']['table']['columns'])) {
-			$this->cache['databaseInfo']['table']['columns'] = $this->getTable()->getColumns();
+		$serialized = array();
+		if (count($array)) {
+			foreach ($array as $k => $v) {
+				$serialized[] = $k . "=\"" . $v . "\"";
+			}
 		}
-		return $this->cache['databaseInfo']['table']['columns'];
+
+		return implode(",", $serialized);
 	}
 
 }
